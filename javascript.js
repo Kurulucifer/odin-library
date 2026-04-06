842ef305bcfdaff8b2ef98af19cf08ee2b33aba9
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-04-06 11:27:29 -0700

    Implement book adding button with form

--- javascript.js
+++ javascript.js
@@ -20,6 +20,26 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+const bookForm = document.getElementById("form-book");
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitHandler();
+});
+
+function submitHandler() {
+    const bookFormData = new FormData(bookForm);
+    const title = bookFormData.get("title");
+    const author = bookFormData.get("author");
+    const pages = bookFormData.get("pages");
+    const readStatus = bookFormData.get("read-status");
+    addBookToLibrary(title, author, pages, readStatus);
+    bookForm.reset();
+}
+
+
+
 // addBookToLibrary("Project: Hail Mary", "Andy Weir", 400, "READ");
 // console.log(myLibrary);
+
+
 
