146d55e7d14ca5d2748d6ff32de0fd1f45650ea2
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-04-15 16:23:52 -0700

    Change Book object to Book class

--- javascript.js
+++ javascript.js
@@ -1,26 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read, uuid) {
-    if (!new.target) {
-        throw Error("You must use the 'new' oeprator to call the constructor.")
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.uuid = crypto.randomUUID();
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.uuid = uuid
 
-    this.info = function() {
+    info() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}, ID: ${this.uuid}`;
     }
 
-    this.toggleRead = function () {
+    toggleRead() {
         this.read = this.read === "Read" ? "Unread" : "Read";
     }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read, crypto.randomUUID());
+    const newBook = new Book(title, author, pages, read);
     myLibrary.unshift(newBook);
 }
 
