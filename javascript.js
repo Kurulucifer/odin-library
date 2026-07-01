26bc12b026908f43edde76c2526dcaa9dd6edc84
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-06-30 17:11:53 -0700

    Change formatting to ESList/Prettier

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
 const myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.uuid = crypto.randomUUID();
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.uuid = crypto.randomUUID();
+  }
 
-    info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}, ID: ${this.uuid}`;
-    }
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}, ID: ${this.uuid}`;
+  }
 
-    toggleRead() {
-        this.read = this.read === "Read" ? "Unread" : "Read";
-    }
+  toggleRead() {
+    this.read = this.read === "Read" ? "Unread" : "Read";
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.unshift(newBook);
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.unshift(newBook);
 }
 
 const bookForm = document.getElementById("form-book");
@@ -28,117 +28,113 @@ const booksContainer = document.getElementById("library");
 const addBookContainer = document.getElementById("add-book-container");
 
 bookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    submitHandler();
+  e.preventDefault();
+  submitHandler();
 });
 
 booksContainer.addEventListener("click", (e) => {
-    if (e.target.className === "remove-button")
-        removeBook(e.target.parentElement.parentElement);
-})
+  if (e.target.className === "remove-button")
+    removeBook(e.target.parentElement.parentElement);
+});
 
 booksContainer.addEventListener("click", (e) => {
-    if (e.target.className === "toggle-read")
-        toggleRead(e.target.parentElement.parentElement);
-})
+  if (e.target.className === "toggle-read")
+    toggleRead(e.target.parentElement.parentElement);
+});
 
 function submitHandler() {
-    const bookFormData = new FormData(bookForm);
-    const title = bookFormData.get("title");
-    const author = bookFormData.get("author");
-    const pages = bookFormData.get("pages");
-    const readStatus = bookFormData.get("read-status");
+  const bookFormData = new FormData(bookForm);
+  const title = bookFormData.get("title");
+  const author = bookFormData.get("author");
+  const pages = bookFormData.get("pages");
+  const readStatus = bookFormData.get("read-status");
 
-    addBookToLibrary(title, author, pages, readStatus);
-    const bookEntry = createBookEntry(myLibrary[0]);
-    booksContainer.prepend(bookEntry);
+  addBookToLibrary(title, author, pages, readStatus);
+  const bookEntry = createBookEntry(myLibrary[0]);
+  booksContainer.prepend(bookEntry);
 
-    booksContainer.insertBefore(addBookContainer, booksContainer.firstChild);
+  booksContainer.insertBefore(addBookContainer, booksContainer.firstChild);
 
-    bookForm.reset();
+  bookForm.reset();
 }
 
 function createBookEntry(book) {
-    const newBookEntry = document.createElement("div");
-    newBookEntry.className = "book";
-    newBookEntry.dataset.uuid = book.uuid;
+  const newBookEntry = document.createElement("div");
+  newBookEntry.className = "book";
+  newBookEntry.dataset.uuid = book.uuid;
 
-    const title = document.createElement("div");
-    const author = document.createElement("div");
-    const pages = document.createElement("div");
-    const read = document.createElement("div");
-    const uuid = document.createElement("div");
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const pages = document.createElement("div");
+  const read = document.createElement("div");
+  const uuid = document.createElement("div");
 
-    title.className = "title";
-    author.className = "author";
-    pages.className = "pages";
-    read.className = "read";
-    uuid.className = "uuid";
+  title.className = "title";
+  author.className = "author";
+  pages.className = "pages";
+  read.className = "read";
+  uuid.className = "uuid";
 
-    title.textContent = book.title;
-    author.textContent = `by ${book.author}`;
-    pages.textContent = `${book.pages} pages`;
-    read.textContent = book.read;
-    uuid.textContent = book.uuid;
+  title.textContent = book.title;
+  author.textContent = `by ${book.author}`;
+  pages.textContent = `${book.pages} pages`;
+  read.textContent = book.read;
+  uuid.textContent = book.uuid;
 
-    newBookEntry.appendChild(title);
-    newBookEntry.appendChild(author);
-    newBookEntry.appendChild(pages);
-    newBookEntry.appendChild(read);
-    newBookEntry.appendChild(uuid);
+  newBookEntry.appendChild(title);
+  newBookEntry.appendChild(author);
+  newBookEntry.appendChild(pages);
+  newBookEntry.appendChild(read);
+  newBookEntry.appendChild(uuid);
 
-    const buttonContainer = document.createElement("div");
-    buttonContainer.className = "button-container";
-    newBookEntry.appendChild(buttonContainer);
+  const buttonContainer = document.createElement("div");
+  buttonContainer.className = "button-container";
+  newBookEntry.appendChild(buttonContainer);
 
-    const toggleRead = document.createElement("button");
-    toggleRead.className = "toggle-read";
-    toggleRead.textContent = "Toggle Read";
-    buttonContainer.appendChild(toggleRead);
+  const toggleRead = document.createElement("button");
+  toggleRead.className = "toggle-read";
+  toggleRead.textContent = "Toggle Read";
+  buttonContainer.appendChild(toggleRead);
 
-    const removeButton = document.createElement("button");
-    removeButton.className = "remove-button";
-    removeButton.textContent = "Remove";
-    buttonContainer.appendChild(removeButton);
+  const removeButton = document.createElement("button");
+  removeButton.className = "remove-button";
+  removeButton.textContent = "Remove";
+  buttonContainer.appendChild(removeButton);
 
-    return newBookEntry;
+  return newBookEntry;
 }
 
 function removeBook(bookEntry) {
-    for (const book of myLibrary) {
-        if (book.uuid === bookEntry.dataset.uuid) {
-            const index = myLibrary.indexOf(book);
-            myLibrary.splice(index, 1);
-            break;
-        }
+  for (const book of myLibrary) {
+    if (book.uuid === bookEntry.dataset.uuid) {
+      const index = myLibrary.indexOf(book);
+      myLibrary.splice(index, 1);
+      break;
     }
+  }
 
-    bookEntry.remove();
+  bookEntry.remove();
 }
 
 function toggleRead(bookEntry) {
-    for (const book of myLibrary) {
-        if (book.uuid === bookEntry.dataset.uuid) {
-            book.toggleRead();
-            bookEntry.replaceWith(createBookEntry(book));
-            break;
-        }
+  for (const book of myLibrary) {
+    if (book.uuid === bookEntry.dataset.uuid) {
+      book.toggleRead();
+      bookEntry.replaceWith(createBookEntry(book));
+      break;
     }
+  }
 }
 
 // For debugging purposes
 function rebuildLibrary(bookEntry) {
-    const myLibraryCopy = myLibrary.slice().reverse();
-    for (const book of myLibraryCopy) {
-        const bookEntry = createBookEntry(book);
-        booksContainer.prepend(bookEntry);
-        booksContainer.insertBefore(addBookContainer, booksContainer.firstChild);
-    }
+  const myLibraryCopy = myLibrary.slice().reverse();
+  for (const book of myLibraryCopy) {
+    const bookEntry = createBookEntry(book);
+    booksContainer.prepend(bookEntry);
+    booksContainer.insertBefore(addBookContainer, booksContainer.firstChild);
+  }
 }
 
 addBookToLibrary("Project: Hail Mary", "Andy Weir", 496, "Read");
 rebuildLibrary();
-
-
-
-
